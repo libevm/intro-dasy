ec6703ac622457270df5901033fe403fc496c32d
Ubuntu <user@ubuntu.com>
2024-01-22 22:15:49 +1100

    ir

---
+++ test_ir.py
@@ -0,0 +1,18 @@
+import json
+import boa
+
+from dasy import compiler
+from dasy.parser.output import get_external_interface
+
+source_code = 'ir.dasy'
+
+with open(f'./{source_code}') as f:
+    src = f.read()
+    compiler_data = compiler.compile(src, name=source_code.split('.')[0])
+
+contract_addr = boa.env.deploy_code(bytecode=compiler_data.bytecode)[0]
+contract_factory = boa.loads_abi(json.dumps(compiler_data.abi))
+contract = contract_factory.at(contract_addr)
+
+# Gets the random number
+print(contract.slot0())
